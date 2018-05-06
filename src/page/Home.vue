5af0bef248bc5fd1820dc1574f0e5762ff769649
shanghui-yu <1150322487@qq.com>
2018-05-06 20:19:32 +0800

    fix:修复表格高度问题

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -8,6 +8,9 @@
 .ivu-layout-content{
   margin:20px auto;
 }
+.ivu-card-body .content{
+  height: calc(100vh - 175px);
+}
 </style>
 <template>
     <div class="layout">
@@ -22,8 +25,8 @@
             </Header>
             <Content :style="{padding: '0 50px'}">
                 <Card>
-                    <div class="content">
-                      <Table class="ivu-table-auto" border height="700" :columns="columns" :data="allData"></Table>
+                    <div class="content" ref="table">
+                      <Table class="ivu-table-auto" border :height="wh" :columns="columns" :data="allData"></Table>
                     </div>
                     <div style="margin-top:20px;">
                       <Page :total='total' :page-size="20" show-total @on-change="pageChange"></Page>
@@ -34,62 +37,66 @@
     </div>
 </template>
 <script>
-import XHR from "../api";
-    export default {
-        data(){
-          return {
-            columns: [
-              {
-                title: '用户名',
-                key: 'nickname'
-              },
-              {
-                title: '省份',
-                key: 'country'
-              },
-              {
-                title: '城市',
-                key: 'city'
-              },
-              {
-                title: '用户openid',
-                key: 'uid'
-              },
-              {
-                title: '手机号',
-                key: 'phone'
-              },
-              {
-                title: '性别',
-                key: 'sex'
-              }
-            ],// table数据内容
-            allData: [],
-            page:0,
-            total:0
-          }
+import XHR from '../api'
+export default {
+  data () {
+    return {
+      columns: [
+        {
+          title: '用户名',
+          key: 'nickname'
         },
-        created(){
-          this.getUserList()
+        {
+          title: '省份',
+          key: 'country'
         },
-        methods: {
-            getUserList(){
-              let json = {
-                page:this.page,
-                size:20
-              }
-              XHR.getUserList(json).then(res=>{
-                let {status,data,total}=res.data
-                this.total = total
-                if(!status){
-                  this.allData = data
-                }
-              })
-            },
-            pageChange(e){
-              this.page = e - 1
-              this.getUserList()
-            }
+        {
+          title: '城市',
+          key: 'city'
+        },
+        {
+          title: '用户openid',
+          key: 'uid'
+        },
+        {
+          title: '手机号',
+          key: 'phone'
+        },
+        {
+          title: '性别',
+          key: 'sex'
         }
+      ], // table数据内容
+      allData: [],
+      page: 0,
+      wh: 400,
+      total: 0
     }
+  },
+  created () {
+    this.getUserList()
+  },
+  mounted () {
+    this.wh = this.$refs.table.offsetHeight
+  },
+  methods: {
+    getUserList () {
+      let json = {
+        page: this.page,
+        size: 20
+      }
+      XHR.getUserList(json).then(res => {
+        let {status, data, total} = res.data
+        this.total = total
+        if (!status) {
+          this.allData = data
+        }
+      })
+    },
+    pageChange (e) {
+      this.page = e - 1
+      this.getUserList()
+    }
+  }
+}
 </script>
